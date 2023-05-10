8156d41fa4153ce4beba91396dd652da7ca53477
salik2010 <salomonsalik1993@mail.ru>
2023-05-10 23:11:32 +0700

    1.6.1 Finish RestApi

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import ru.kata.spring.boot_security.demo.security.UserDetailsServiceImp;
 import ru.kata.spring.boot_security.demo.service.UserService;
 import ru.kata.spring.boot_security.demo.service.UserServiceImp;
 
@@ -26,16 +27,19 @@ import javax.sql.DataSource;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final SuccessUserHandler successUserHandler;
 
+    private final UserDetailsService userDetailsService;
+
     private final UserServiceImp userServiceimp;
 
-    public WebSecurityConfig(SuccessUserHandler successUserHandler, UserServiceImp userServiceimp) {
+    public WebSecurityConfig(SuccessUserHandler successUserHandler, UserServiceImp userServiceimp, UserDetailsService userDetailsService) {
         this.successUserHandler = successUserHandler;
         this.userServiceimp = userServiceimp;
+        this.userDetailsService = userDetailsService;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userServiceimp);
+        auth.userDetailsService(userDetailsService);
     }
 
     @Override
